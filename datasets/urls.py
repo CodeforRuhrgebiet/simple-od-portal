0ffe0687b0582a937b7e827a8fda5e138cd2dc0d
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-03-10 10:03:08 +0100

    Implement simple search.

--- datasets/urls.py
+++ datasets/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
 
-from .views import DatasetListView, DatasetDetailView
+from .views import DatasetListView, DatasetDetailView, DatasetSearchView
 
 
 urlpatterns = [
     url(r'^$', DatasetListView.as_view(), name='dataset-list'),
+    url(r'^%s/$' % _('search'), DatasetSearchView.as_view(), name='dataset-search'),
     url(r'^(?P<slug>[-\w]+)/$', DatasetDetailView.as_view(), name='dataset-detail'),
 ]
--- datasets/views.py
+++ datasets/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 from django.shortcuts import redirect
 
@@ -10,6 +11,24 @@ class DatasetListView(ListView):
 
 class DatasetDetailView(DetailView):
     model = Dataset
+
+
+class DatasetSearchView(ListView):
+    model = Dataset
+    template_name = 'datasets/search_results.html'
+
+    def get_queryset(self):
+        q = self.request.GET.get('q', None)
+        if q and len(q) > 3:
+            qs = super().get_queryset().filter(
+                Q(name__icontains=q) |
+                Q(tags_raw__icontains=q) |
+                Q(description__icontains=q) |
+                Q(file_content__icontains=q)
+            ).order_by('name').distinct()
+            if qs:
+                return qs
+        return []
 
 
 def index(request, *args, **kwargs):
